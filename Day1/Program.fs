679c7d249876a86a0a8a0472f74507cd703b788d
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-12-11 09:40:15 +0100

    Day 1: parse input to `int option list` at the beginning

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -14,34 +14,32 @@
       ""
       "10000" ]
 
-let splitList (calories: string list) : string list list =
-    let result = 
+let parseInput input =
+    List.map (fun s -> if s = "" then None else Some(int s)) input
+
+let splitList (calories: int option list) : int list list =
+    let result =
         calories
-        |> List.fold (fun (acc: string list list) (calorie: string) ->
-            if calorie = "" then
-                // append new empty list to acc
-                [] :: acc
-            else
-                match acc with
-                // if acc is empty, append new list with calorie to acc
-                | [] -> [[calorie]]
-                // append calorie to first list in acc (which is the last list modified)
-                | head :: tail -> (calorie :: head) :: tail
-            ) []
-        
+        |> List.fold
+            (fun (acc: int list list) (calorie: int option) ->
+                match calorie with
+                | None -> [] :: acc
+                | Some calorie ->
+                    match acc with
+                    // if acc is empty, append new list with calorie to acc
+                    | [] -> [ [ calorie ] ]
+                    // append calorie to first list in acc (which is the last list modified)
+                    | head :: tail -> (calorie :: head) :: tail)
+            [] // <-- initial value of acc
+
     result |> List.rev
 
 let allElves =
-    input
-    |> splitList
-    |> List.map (List.map int)
-    |> List.map List.sum
+    input |> parseInput |> splitList |> List.map (List.map int) |> List.map List.sum
 
 let winner = allElves |> List.max
 
 printfn
-    "%d. elf is carrying most calories: %d"
-    (allElves
-     |> List.findIndex (fun x -> x = winner)
-     |> (fun x -> x + 1))
+    "#%d elf is carrying most calories: %d"
+    (allElves |> List.findIndex (fun x -> x = winner) |> (fun x -> x + 1))
     winner
