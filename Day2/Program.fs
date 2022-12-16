773a5af9133168a1a01a739bdedfcee04168631f
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-12-16 13:58:57 +0100

    Day 2: complete part two

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -30,3 +30,25 @@ let getScore = function
 let totalScore = input |> List.map getScore |> List.sum
 
 printfn "Total score: %d" totalScore
+
+// --- Part Two ---
+
+// X - lose
+// Y - draw
+// Z - win
+
+let getScore2 = function
+    | "A X" -> 3 + 0
+    | "A Y" -> 1 + 3
+    | "A Z" -> 2 + 6
+    | "B X" -> 1 + 0
+    | "B Y" -> 2 + 3
+    | "B Z" -> 3 + 6
+    | "C X" -> 2 + 0
+    | "C Y" -> 3 + 3
+    | "C Z" -> 1 + 6
+    | _ -> failwith "Invalid input"
+    
+let totalScore2 = input |> List.map getScore2 |> List.sum
+
+printfn "Total score: %d" totalScore2
