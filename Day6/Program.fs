e0daa7e0f6997a9b5f006c754e48c8ad4714e083
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-12-22 19:14:03 +0100

    Day 6: Complete part 1

---
+++ Day6/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.IO
+
+let input = File.ReadAllLines $@"C:\Users\{Environment.UserName}\Desktop\input.txt" |> Array.exactlyOne
+
+let signalSubroutine signal =
+    let rec loop (index: int) (acc: char list) : int =
+        if [ acc[0]; acc[1]; acc[2]; acc[3] ] |> List.distinct |> List.length = 4 then
+            index + 4
+        else
+            loop (index + 1) (List.tail acc)
+            
+    loop 0 signal
+
+input
+|> List.ofSeq
+|> signalSubroutine
+|> printfn "%d"
