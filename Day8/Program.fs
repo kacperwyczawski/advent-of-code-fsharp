8187b293c24d661dc64bdb5dd04d9f072106c208
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-12-29 15:15:15 +0100

    Day 8: Complete part 2

--- Day8/Program.fs
+++ Day8/Program.fs
@@ -1,8 +1,14 @@
 ﻿open Utilities
 
-let input = Input.loadLines
+let parseInput (input: string list) =
+    let charToInt c = int c - 48
+    input |> List.map List.ofSeq |> List.map (List.map charToInt)
+    
+let input = parseInput Input.loadLines
 
-type View =
+// --- Part One ---
+
+type OutsideView =
     | Top of int list list
     | Bottom of int list list
     | Left of int list list
@@ -17,23 +23,14 @@ type Point = { X: int; Y: int }
 // distinct concatenated list
 // count
 
-let parseInput (input: string list) =
-    let charToInt c = int c - 48
-
-    let raw: int list list =
-        input |> List.map List.ofSeq |> List.map (List.map charToInt)
-
-    let left = Left raw
-
-    let right = raw |> List.map List.rev |> Right
-
-    let top = raw |> List.transpose |> Top
-
-    let bottom = raw |> List.transpose |> List.map List.rev |> Bottom
-
+let getOutsideViews grid =
+    let left = Left grid
+    let right = grid |> List.map List.rev |> Right
+    let top = grid |> List.transpose |> Top
+    let bottom = grid |> List.transpose |> List.map List.rev |> Bottom
     [ left; right; top; bottom ]
 
-let getVisibleTrees (view: View) =
+let getVisibleTrees (view: OutsideView) =
     let getInLine pointModifier startingPoint grid =
         let rec visibleInLine line highest visibleTrees point =
             match line with
@@ -48,20 +45,80 @@ let getVisibleTrees (view: View) =
                         visibleInLine tail (Some h) visibleTrees (pointModifier point)
 
         grid |> List.mapi (fun i line -> visibleInLine line None [] (startingPoint i))
-        
+
     match view with
     | Top grid -> getInLine (fun p -> { p with Y = p.Y + 1 }) (fun i -> { X = i; Y = 0 }) grid
     | Bottom grid -> getInLine (fun p -> { p with Y = p.Y - 1 }) (fun i -> { X = i; Y = grid.Length - 1 }) grid
     | Left grid -> getInLine (fun p -> { p with X = p.X + 1 }) (fun i -> { X = 0; Y = i }) grid
     | Right grid -> getInLine (fun p -> { p with X = p.X - 1 }) (fun i -> { X = grid.Length - 1; Y = i }) grid
 
-let solution =
+let solutionOne =
     input
-    |> parseInput
+    |> getOutsideViews
     |> List.map getVisibleTrees // views, lines and points
     |> List.concat // views and points
     |> List.concat // points
     |> List.distinct
     |> List.length
 
-printfn "%d" solution
+printfn "Part 1: %d" solutionOne
+
+// --- Part Two ---
+
+type TreeHouseSpot =
+    { Height: int
+      ViewTop: int list
+      ViewBottom: int list
+      ViewLeft: int list
+      ViewRight: int list }
+
+let getTreeHouseSpots forest =
+    let height = forest |> List.length
+    let width = forest |> List.head |> List.length
+    
+    let rec getView (forest: int list list) x y acc breakCondition coordsModifier =
+        if breakCondition x y then
+            acc |> List.rev
+        else
+            let updatedX, updatedY = coordsModifier x y
+            getView forest updatedX updatedY (forest[y][x] :: acc) breakCondition coordsModifier
+
+    let rec getViews x y acc =
+        if y >= height then // if we are at the bottom of the forest, we are done
+            acc
+        else if x >= width then // if we are at the end of the line, go to the next line
+            getViews 0 (y + 1) acc
+        else
+            let newSpot =
+                { Height = forest[y][x]
+                  ViewTop = getView forest x (y - 1) [] (fun _ y -> y < 0) (fun x y -> x, y - 1)
+                  ViewBottom = getView forest x (y + 1) [] (fun _ y -> y >= height) (fun x y -> x, y + 1)
+                  ViewLeft = getView forest (x - 1) y [] (fun x _ -> x < 0) (fun x y -> x - 1, y)
+                  ViewRight = getView forest (x + 1) y [] (fun x _ -> x >= width) (fun x y -> x + 1, y) }
+
+            getViews (x + 1) y (newSpot :: acc)
+
+    getViews 0 0 []
+
+let getScenicScore spot =
+    let viewingDistance height line =
+        line
+        |> List.tryFindIndex (fun x -> x >= height)
+        |> (function
+        | Some x -> x + 1
+        | None -> line.Length)
+
+    let top = spot.ViewTop |> viewingDistance spot.Height
+    let bottom = spot.ViewBottom |> viewingDistance spot.Height
+    let left = spot.ViewLeft |> viewingDistance spot.Height
+    let right = spot.ViewRight |> viewingDistance spot.Height
+
+    top * bottom * left * right
+
+let solutionTwo =
+    input
+    |> getTreeHouseSpots
+    |> List.map getScenicScore
+    |> List.max
+    
+printfn "Part 2: %d" solutionTwo
