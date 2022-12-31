1a4c869a95d02edbb8209f3b85c3b182c94e52a4
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-12-31 16:14:10 +0100

    Add loading test file feature in Utilities

--- Utilities/Input.fs
+++ Utilities/Input.fs
@@ -4,5 +4,27 @@ open System
 open System.IO
 
 module Input =
-    let loadLines = File.ReadAllLines $@"C:\Users\{Environment.UserName}\Desktop\input.txt" |> List.ofArray
-    let loadText = File.ReadAllText $@"C:\Users\{Environment.UserName}\Desktop\input.txt"
+    let private desktopPath =
+        Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+
+    let loadLines =
+        [| desktopPath; "input.txt" |]
+        |> Path.Combine
+        |> File.ReadAllLines
+        |> List.ofArray
+
+    let loadTestLines =
+        [| desktopPath; "test.txt" |]
+        |> Path.Combine
+        |> File.ReadAllLines
+        |> List.ofArray
+
+    let loadText =
+        [| desktopPath; "input.txt" |]
+        |> Path.Combine
+        |> File.ReadAllText
+
+    let loadTestText =
+        [| desktopPath; "test.txt" |]
+        |> Path.Combine
+        |> File.ReadAllText
